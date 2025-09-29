6a4d031209a97012659d68f0031cc5e4158d2004
Your Name <deepakash7051@gmail.com>
2025-09-29 20:39:38 +0530

    new changes

--- datatypes/fundamental_datatypes.py
+++ datatypes/fundamental_datatypes.py
@@ -107,3 +107,47 @@ print(f'Hello {name}, you are {age} year old')
 print('Hi {}, you are {} years old'.format('Akash',32))
 
 # String Index
+selfish = 'me me me'
+print(selfish[3])
+
+# [start:stop]
+str = "01234567"
+print(str[0:2])
+
+# [start: end: stepover]
+print(str[1:8:2])
+print(str[1:])
+print(str[:5])
+print(str[::1])
+print(str[-1])
+print(str[::-1])
+print(str[-2])
+print(str[::-2])
+
+# immutability (can not be chnage)
+strg = '13213213212'
+print(strg)
+# strg[2] = 5
+# print(strg)
+
+# Some in build functions
+print( len('helloooo') )
+
+quote = 'to be or not to be'
+print(quote.upper())
+print(quote.capitalize())
+print(quote.find('be'))
+print(quote.replace('be', 'me'))
+print(quote)
+
+# Booleans (TRUE/FALSE)
+name = 'Andrei'
+is_cool = False
+is_cool = True
+print(name)
+
+
+birthYear = input('What is your birth year ')
+age = 2025 - int(birthYear)
+
+print(f'Your age is {age}')
